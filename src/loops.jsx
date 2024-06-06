5676dd3cbbde789623572d131e8b5fb93b77134c
RyanD <ryanperera0531@gmail.com>
2024-06-06 17:17:29 +0530

    updated

--- src/loops.jsx
+++ src/loops.jsx
@@ -4,7 +4,7 @@
 //     console.log(i)
 // }
 
-
+//while loop
 // let i = 1;
 
 // while(i<=10){
@@ -12,15 +12,36 @@
 //     i++
 // }
 
-
+//do-while loop
 // let i = 0;
 // do{
 //     console.log(i)
 //     i++
 // }while(i<10)
 
+
+//foreach loop
 const numbers = [10,3,1,2]
 
 numbers.forEach((number,i) => {
     console.log(i,number)
 })
+
+//for in loop
+for(let number in numbers){
+    console.log(numbers[number])
+}
+
+//for of loop
+for(let number of numbers){
+    console.log(number)
+}
+
+const employee = {
+    name : "Ryan",
+    age : 10
+}
+
+for(let value of employee){
+    console.log(value)
+}
